b5832433aa6cd8f3af70f4d30290a4c2b18828a7
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2025-04-19 21:32:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-SnapExtensions.set("ide_cat_setcategory(string categoryname,Color categorycolor)",function(name,color){
+SnapExtensions.primitives.set("ide_cat_setcategory(string categoryname,Color categorycolor)",function(name,color){
   world.childThatIsA(IDE_Morph).scene.customCategories.set(name,color)
 })
-SnapExtensions.set("ide_cat_getcategory(string categoryname)->Color",function(name){
+SnapExtensions.primitives.set("ide_cat_getcategory(string categoryname)->Color",function(name){
   return world.childThatIsA(IDE_Morph).scene.customCategories.get(name)
 })
-SnapExtensions.set("ide_cat_getcategories()->string[]",function(name){
+SnapExtensions.primitives.set("ide_cat_getcategories()->string[]",function(name){
   return world.childThatIsA(IDE_Morph).scene.customCategories.keys()
 })
-SnapExtensions.set("ide_cat_removecategory(string categoryname)->boolean",function(name){
+SnapExtensions.primitives.set("ide_cat_removecategory(string categoryname)->boolean",function(name){
   return world.childThatIsA(IDE_Morph).scene.customCategories.delete(name)
 })
